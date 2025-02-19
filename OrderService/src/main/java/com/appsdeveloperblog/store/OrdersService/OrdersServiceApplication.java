2b6909dbc486bd73f47eba9a2e62f9b7cf3afece
mangelg1984 <mangelg1984.1@gmail.com>
2025-02-19 10:49:16 -0600

    Root directories

---
+++ OrderService/src/main/java/com/appsdeveloperblog/store/OrdersService/OrdersServiceApplication.java
@@ -0,0 +1,16 @@
+package com.appsdeveloperblog.store.OrdersService;
+
+import configuration.AxonConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@Import({AxonConfig.class})
+public class OrdersServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersServiceApplication.class, args);
+	}
+}
